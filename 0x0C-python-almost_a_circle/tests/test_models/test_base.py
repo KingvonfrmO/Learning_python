31e008c4aa3ae51fad05a1ddf4267b75de017b72
Jeffery mutuku <2205481@students.kcau.ac.ke>
2024-06-29 14:39:23 +0300

    This is my Twelve assignment

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,14 +3,6 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-
-    def setUp(self):
-        Base._base__nb_objects = 0
-        pass
-
-    def tearDown(self):
-        pass
-
     def test_no_id(self):
         b1 = Base()
         b2 = Base()
@@ -21,15 +13,17 @@ class TestBase(unittest.TestCase):
         b3 = Base(10)
         self.assertEqual(b3.id, 10)
 
+    def setUp(self):
+        # Reset the counter before each test
+        Base._Base__nb_objects = 0
+
     def test_mixed(self):
         b4 = Base()
         b5 = Base(20)
         b6 = Base()
-
-        self.assertEqual(b4.id, 3)
+        self.assertEqual(b4.id, 1)
         self.assertEqual(b5.id, 20)
-        self.assertEqual(b6.id, 4)
-
+        self.assertEqual(b6.id, 2)
 
 if __name__ == "__main__":
     unittest.main()
