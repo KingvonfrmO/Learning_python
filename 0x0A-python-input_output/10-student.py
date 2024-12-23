4c2718091e3b2d69176e965667bc36b655a1fa20
KingvonfrmO <2205481@students.kcau.ac.ke>
2024-12-23 19:40:46 +0300

    ASSIGNMENT 10

---
+++ 0x0A-python-input_output/10-student.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
